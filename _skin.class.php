09e65ec56624c2384b305a8146aaf3058dfd44af
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-11-02 07:41:25 +0100

    skin update

--- _skin.class.php
+++ _skin.class.php
@@ -8,7 +8,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 /**
  * Specific code for this skin.
  */
-class AllAboutPets_Skin extends Skin
+class allaboutpets_Skin extends Skin
 {
 	var $version = '1.1.1';
 
